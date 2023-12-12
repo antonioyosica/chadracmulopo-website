af26519897760b12b675dacd1ade75adfc5b3a14
António Yosica <antonioyosica@gmail.com>
2023-12-12 15:21:37 +0100

    Home: changed acording to the new design [bug fixed #1]

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,15 +19,13 @@ useHead({
           <span>Apaixonado pela animação aplicada à publicidade e branding.</span>
         </div>
       </div>
-      <div class="col-12 col-sm-12 col-md-3 about">
-        <div class="home-card2">
-          <a href="#">Sobre&nbsp;<strong>mim</strong></a>
-        </div>
+      <div class="col-12 col-sm-12 col-md-2 about">
+        <div class="home-card2"></div>
+        <a href="#">Sobre&nbsp;<strong>mim</strong></a>
       </div>
-      <div class="col-12 col-sm-12 col-md-4 work">
-        <div class="home-card3">
-          <a href="#">Meus&nbsp;<strong>trabalhos</strong></a>
-        </div>
+      <div class="col-12 col-sm-12 col-md-5 work">
+        <div class="home-card3"></div>
+        <a href="#">Meus&nbsp;<strong>trabalhos</strong></a>
       </div>
     </div>
   </main>
@@ -55,7 +53,7 @@ main .container-fluid .home-card2 {
   background-size: 100% 125%;
   transition: transform 0.5s ease-in-out;
 }
-main .container-fluid .home-card2:hover {
+main .container-fluid a:hover + main .container-fluid .home-card2 {
   transform: scale(1.3);
 }
 main .container-fluid .home-card3 {
@@ -87,8 +85,7 @@ main .container-fluid .home-card1 span {
   font-size: 1.4em;
   line-height: 1.3 !important;
 }
-main .container-fluid .home-card2 a,
-main .container-fluid .home-card3 a{
+main .container-fluid a{
   font-size: 0.9em;
   line-height: 1.3 !important;
   text-decoration: none;
@@ -99,22 +96,27 @@ main .container-fluid .home-card3 a{
   display: flex;
   align-items: flex-end;
   justify-content: center;
+  position: relative;
+  top: -100%;
+}
+main .container-fluid a:hover{
+  text-decoration: underline;
 }
 
 @media (min-width: 992px) {
   main .container-fluid .home-card1 span {
     font-size: 1.6em;
   }
-  main .container-fluid .home-card2 a,
-  main .container-fluid .home-card3 a{
+  main .container-fluid a,
+  main .container-fluid a{
     font-size: 1em;
     padding: 36px;
   }
-  main .container-fluid .home-card2 a {
+  main .container-fluid .about a {
     align-items: flex-end;
     justify-content: left;
   }
-  main .container-fluid .home-card3 a {
+  main .container-fluid .work a {
     align-items: flex-end;
     justify-content: right;
   }
@@ -133,9 +135,7 @@ main .container-fluid .home-card3 a{
   }
   main .container-fluid .home-card1 {
     margin: 0 20px 0 0 !important;
-    padding-top: 125px !important;
-    justify-content: left;
-    text-align: left;
+    padding: 125px 64px 0 !important;
   }
 }
 </style>
